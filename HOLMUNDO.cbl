5bfb854f0bc093df1417e8bcdb01eb26f0ac7f20
DANIEL CAZABAT <dacazabat@gmail.com>
2025-06-30 09:20:20 -0300

    Fix APAREO Name

--- HOLMUNDO.cbl
+++
@@ -1,5 +0,0 @@
-       IDENTIFICATION DIVISION.                                         00010002
-       PROGRAM-ID. HOLMUNDO.                                            00020002
-       PROCEDURE DIVISION.                                              00030002
-           DISPLAY 'HOLA MUNDO, BY KC03C7C'.                             00040003
-           STOP RUN.                                                    00060002
